4d6ad6595b2642443272b48f4adf1e18558faa2d
sujonifty <sujonifty@gmail.com>
2024-02-19 02:21:24 +0600

    completed total price section

--- js/app.js
+++ js/app.js
@@ -10,14 +10,61 @@ function totalCost(elementId, value) {
     const sum = totalCost + parseInt(value);
     return sum;
 }
+// calculation the grand total cost 
+function grandTotal(coupon) {
+    // step-1:same ways to get grand total
+    const getTotalPrice = document.getElementById('total-price');
+    const totalPriceText = getTotalPrice.innerText;
+    const getPrice = parseInt(totalPriceText);
 
+    if (coupon === 'Couple 20') {
+        //set total price  in the grand total container after discount
+        const discount = (getPrice * 20) / 100;
+        setInnerText('grand-total', getPrice - discount);
+    }
+    else if (coupon === 'NEW15') {
+        //set total price  in the grand total container after discount
+        const discount = (getPrice * 15) / 100;
+        setInnerText('grand-total', getPrice - discount);
+    }
+    else {
+        //set total price in the grand total container
+        setInnerText('grand-total', getPrice);
+    }
+}
+
+/*
+// hidden the homeScreen
+const homeScreen = document.getElementById('main');
+    homeScreen.classList.add('hidden');
+    // show the modal
+    const playScreen = document.getElementById('modal');
+    playScreen.classList.remove('hidden');
+*/
 const seats = document.getElementsByClassName('kbd');
 let count = 0;
+const seatText = document.getElementById('total-seat').innerText;
+let totalSeat = parseInt(seatText);
 for (let seat of seats) {
     seat.addEventListener('click', function (event) {
-
+        totalSeat -= 1;
         count += 1;
+        if(count === 1 ){
+            const getPassenger = document.getElementById('passenger-btn');
+            getPassenger.removeAttribute('disabled');
+        }
+        else if (count === 4) {
+            const getApply = document.getElementById('apply-btn');
+            getApply.removeAttribute('disabled');
+            
+        }
+        else if(count > 4){
+            return alert('You can not buy more than 4 tickets');
+        }
         setInnerText('cart-count', count);
+       
+        setInnerText('total-seat', totalSeat);
+        
         // get seat number & ticket price
         const seatName = event.target.innerText;
         const price = document.getElementById('ticket-price').innerText;
@@ -37,22 +84,19 @@ for (let seat of seats) {
         event.target.style.backgroundColor = '#1DD100';
         // set disable button
         event.target.disabled = true;
-
-        //count the booking-seat
-        const seatBookingtext = document.getElementById('cart-count').innerText;
-        const seatBooking = parseInt(seatBookingtext);
-        if(seatBooking === 4){
-            const getApply = document.getElementById('apply-btn').innerText;
-            console.log(getApply);
-        }
+        // calculate the total price
+        const totalPrice = totalCost('total-price', price);
+        // set the total cost in total price container 
+        setInnerText('total-price', totalPrice);
+        // set total cost in the grand total container
+        setInnerText('grand-total', totalPrice);
         
         // step-3: set the element in cart container 
         ul.appendChild(li1);
         ul.appendChild(li2);
         ul.appendChild(li3);
         selectedContainer.appendChild(ul);
-        
-        // console.log('code working');
+           // console.log('code working');
     })
 
 }
