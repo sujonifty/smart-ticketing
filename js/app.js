48581a052a6a48939e821b35bea89bae2ca0be24
sujonifty <sujonifty@gmail.com>
2024-02-19 09:43:29 +0600

    coupon section completed

--- js/app.js
+++ js/app.js
@@ -10,6 +10,40 @@ function totalCost(elementId, value) {
     const sum = totalCost + parseInt(value);
     return sum;
 }
+//apply coupon
+// step-1: get the input field by id 
+const getApplyBtn = document.getElementById('apply-btn');
+getApplyBtn.addEventListener('click', function () {
+    const getCouponField = document.getElementById('input-area');
+    const coupon = getCouponField.value;
+    getCouponField.value = '';
+    // console.log(coupon);
+     // step-1:calculate get grand total
+     const getTotalPrice = document.getElementById('total-price');
+     const totalPriceText = getTotalPrice.innerText;
+     const getPrice = parseInt(totalPriceText);
+     
+     if (coupon === 'Couple 20') {
+         //set total price  in the grand total container after discount
+         const discount = (getPrice * 20) / 100;
+         setInnerText('grand-total', getPrice - discount);
+     }
+     else if (coupon === 'NEW15') {
+         //set total price  in the grand total container after discount
+         const discount = (getPrice * 15) / 100;
+         setInnerText('grand-total', getPrice - discount);
+     }
+     else {
+         //set total price in the grand total container
+         setInnerText('grand-total', getPrice);
+         return alert('Please inter the valid coupon code')
+     }
+    
+});
+
+
+
+/*
 // calculation the grand total cost 
 function grandTotal(coupon) {
     // step-1:same ways to get grand total
@@ -30,9 +64,10 @@ function grandTotal(coupon) {
     else {
         //set total price in the grand total container
         setInnerText('grand-total', getPrice);
+        return alert('Please inter the valid coupon code')
     }
 }
-
+*/
 /*
 // hidden the homeScreen
 const homeScreen = document.getElementById('main');
@@ -49,22 +84,22 @@ for (let seat of seats) {
     seat.addEventListener('click', function (event) {
         totalSeat -= 1;
         count += 1;
-        if(count === 1 ){
+        if (count === 1) {
             const getPassenger = document.getElementById('passenger-btn');
             getPassenger.removeAttribute('disabled');
         }
         else if (count === 4) {
             const getApply = document.getElementById('apply-btn');
             getApply.removeAttribute('disabled');
-            
+
         }
-        else if(count > 4){
+        else if (count > 4) {
             return alert('You can not buy more than 4 tickets');
         }
         setInnerText('cart-count', count);
-       
+
         setInnerText('total-seat', totalSeat);
-        
+
         // get seat number & ticket price
         const seatName = event.target.innerText;
         const price = document.getElementById('ticket-price').innerText;
@@ -90,13 +125,13 @@ for (let seat of seats) {
         setInnerText('total-price', totalPrice);
         // set total cost in the grand total container
         setInnerText('grand-total', totalPrice);
-        
+
         // step-3: set the element in cart container 
         ul.appendChild(li1);
         ul.appendChild(li2);
         ul.appendChild(li3);
         selectedContainer.appendChild(ul);
-           // console.log('code working');
+
     })
 
 }
